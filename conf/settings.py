626244547f1308df2e32ccc5e2c80436598dbaac
SanjarbekAI <sanjarbekwork@gmail.com>
2024-07-09 17:24:45 +0500

    allowed hosts

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
