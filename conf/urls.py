e0e32aff87ca7c337fe73dff1589df38e8ad81ae
SanjarbekAI <sanjarbekwork@gmail.com>
2024-07-09 17:57:20 +0500

    chanegd

--- conf/urls.py
+++ conf/urls.py
@@ -22,11 +22,11 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.urls', namespace='users')),
     path('api/v1/posts/', include('posts.urls', namespace='posts')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
